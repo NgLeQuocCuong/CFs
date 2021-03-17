0eb0714bb07345e6d5453a8da8322a73612c8ac6
Ngcuong <ngcuong25121999@gmail.com>
2021-03-18 00:31:39 +0700

    fix syntax

--- Dataset/Dataset.py
+++ Dataset/Dataset.py
@@ -24,6 +24,6 @@ class Dataset(object):
     def prepare_train_test(self, by_last_rate=True, test_rate=None):
         if test_rate:
             self._train, self._test = train_test_split(self._dataset, test_size=test_rate)
-        else if by_last_rate:
+        elif by_last_rate:
             self._train = self._dataset[self._dataset.groupby('user_id').timestamp.transform(max) != self._dataset['timestamp']]
             self._test = self._dataset[self._dataset.groupby('user_id').timestamp.transform(max) == self._dataset['timestamp']]
