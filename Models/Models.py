7451055fc22cbc5c45e12659a7b41c91355df11e
Ngcuong <ngcuong25121999@gmail.com>
2021-03-22 16:17:43 +0700

    fix DeepCF

--- Models/Models.py
+++ Models/Models.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Input, Mutiply, Dense, Concatenate
+from tensorflow.keras.layers import Input, Multiply, Dense, Concatenate
 from tensorflow.keras import Model
 from tensorflow.keras.metrics import RootMeanSquaredError
 from functools import reduce
@@ -40,8 +40,8 @@ class GMF(CFs):
         i_input = Input(shape=[n_user])
         i_embedding = keras.layers.Embedding(n_user, n_latent_factors)(i_input)
         i_vec = keras.layers.Flatten(name='Flatten_'+names[1])(i_embedding)
-        mutiply = Mutiply()([u_vec, i_vec])
-        output = Dense(1)(mutiply)
+        Multiply = Multiply()([u_vec, i_vec])
+        output = Dense(1)(Multiply)
         self.model = Model([u_input, i_input], output, name='GMF')
         self.model.compile(optimizer='adam', loss='mse',
                            metrics=[RootMeanSquaredError()])
@@ -60,7 +60,7 @@ class DeepCF(CFs):
     def _create_representation_model(self, inputs, representation_layers, activation='relu'):
         user_latent_factor = self.create_mlp(inputs[0], representation_layers, dropout=0.1)
         item_latent_factor = self.create_mlp(inputs[1], representation_layers, dropout=0.1)
-        return Mutiply()([user_latent_factor, item_latent_factor])
+        return Multiply()([user_latent_factor, item_latent_factor])
 
     def _create_matchingfunction_model(self, inputs, embedding_size=16, matching_layers = [32], activation='relu'):
         user_latent_factor = Dense(embedding_size, activation=activation)(inputs[0])
