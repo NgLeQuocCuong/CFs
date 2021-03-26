ffa03d70ca4f865ddb7bc3607cf7ffbf0c5503d4
Ngcuong <ngcuong25121999@gmail.com>
2021-03-26 14:28:28 +0700

    fix dataset

--- Dataset/Dataset.py
+++ Dataset/Dataset.py
@@ -44,11 +44,11 @@ class Dataset(object):
 
     def load_item_data(self, filename):
         self.item_input = read_csv(filename)
-        self.item_input.data = self.item_input.data.apply(lambda x: x.split())
+        self.item_input.data = self.item_input.data.apply(lambda x: np.array(x.split()))
 
     def load_user_data(self, filename):
         self.user_input = read_csv(filename)
-        self.user_input.data = self.user_input.data.apply(lambda x: x.split())
+        self.user_input.data = self.user_input.data.apply(lambda x: np.array(x.split()))
 
     def prepare_train_test(self, by_last_rate=True, test_rate=None):
         if test_rate:
